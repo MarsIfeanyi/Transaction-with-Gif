827a6296fb059ee1ab5f8123acf3484e6e69d1ff
MarsIfeanyi <marschalice@gmail.com>
2023-05-11 12:58:06 +0100

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-blue-600 text-3xl mt-10 text-center mx-auto">
+        <h1>Transaction With Gif</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
